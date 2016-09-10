32e69780b83a6bd62adc8cffef31dbd776ff4841
HARVEY BRYCE DAMOLE <HRDAMOLE@fit.edu.ph>
2016-09-10 20:45:59 +0800

    Prints string as an array

--- lexicographicGenerator/src/lexicographicgenerator/LexicographicGenerator.java
+++ lexicographicGenerator/src/lexicographicgenerator/LexicographicGenerator.java
@@ -1,11 +1,12 @@
-
 package lexicographicgenerator;
-
 
 public class LexicographicGenerator {
 
     public static void main(String[] args) {
-   
+        String charSet = "ABC";
+        for (int i = 0; i < charSet.length(); i++) {
+            System.out.println(charSet.charAt(i));
+        }
     }
-    
+
 }
